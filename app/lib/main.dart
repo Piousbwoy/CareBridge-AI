bb30f8577cd5acda044c7a80111f7ad7bf5e3821
CareBridge Developer <dev@carebridge.ai>
2026-07-30 09:55:20 +0000

    fix: add GoogleFonts import to main.dart for clean compilation

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'core/theme.dart';
 import 'data/mock_repository.dart';
 import 'data/sync_queue_service.dart';
